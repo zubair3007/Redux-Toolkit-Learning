c42df8bb190a153c27e7be79d7756b05a99d0dbb
Zubair Khan <kzubair129@gmail.com>
2023-08-14 22:22:48 +0530

    Counter App with redxtoolkit completed

---
+++ Counter-App-with-reduxToolkit/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './features/counter/Counter';
+
+
+const App = () => {
+  return (
+    <main className='App'>
+   <Counter />
+   </main>
+  )
+}
+
+export default App
